b7caaca0641b5f0f5f8666e06b972964130beec9
Fede Trujillo <fede4895@gmail.com>
2025-05-17 16:56:39 -0300

    Modificacion de la clase

--- src/main/java/com/universidad/flota/controller/EncargadoController.java
+++ src/main/java/com/universidad/flota/controller/EncargadoController.java
@@ -1,29 +1,47 @@
 package com.universidad.flota.controller;
 
-import com.universidad.flota.domain.Aprobacion;
+import com.universidad.flota.domain.SolicitudViaje;
 import com.universidad.flota.service.AprobacionService;
+import com.universidad.flota.repository.SolicitudViajeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.awt.*;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/encargado")
 public class EncargadoController {
 
     @Autowired
+    private SolicitudViajeRepository solicitudViajeRepo;
+
+    @Autowired
     private AprobacionService aprobacionService;
 
-    @PostMapping("/aprobar/{solicitudId}")
-    public ResponseEntity<Aprobacion> aprobarSolicitud(@PathVariable Long solicitudId,
-                                                       @RequestParam(required = false) String comentarios) {
-        Aprobacion aprobacion = aprobacionService.aprobarSolicitud(solicitudId);
-        return ResponseEntity.ok(aprobacion);
+    @PostMapping("/solicitudes/{id}/aprobar")
+    public void aprobarSolicitud(@PathVariable Long id,
+                                 @RequestParam(required = false) String comentarios) {
+
+        Optional<SolicitudViaje> solicitudopt = solicitudViajeRepo.findById(id);
+        if (solicitudopt.isPresent()) {
+            aprobacionService.aprobar(solicitudopt.get(), comentarios);
+        } else {
+            throw new RuntimeException("Solicitud no encontrada");
+        }
     }
 
-    @PostMapping("/rechazar/{solicitudId}")
-    public ResponseEntity<Aprobacion> rechazarSolicitud(@PathVariable Long solicitudId,
-                                                        @RequestParam(required = false) String comentarios) {
-        Aprobacion aprobacion = aprobacionService.rechazarSolicitud(solicitudId);
-        return ResponseEntity.ok(aprobacion);
+    @PostMapping("/solicitudes/{id}/rechazar")
+    public void rechazarSolicitud(@PathVariable Long id,
+                                  @RequestParam(required = false) String comentarios) {
+        Optional<SolicitudViaje> solicitudopt = solicitudViajeRepo.findById(id);
+        if (solicitudopt.isPresent()) {
+            aprobacionService.rechazar(solicitudopt.get(), comentarios);
+        } else {
+            throw new RuntimeException("Solicitud no encontrada");
+        }
     }
 }
